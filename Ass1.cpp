28f87cfdf0d18b9aecfaf9d1f3e044ee01e8bac2
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2022-04-07 23:42:07 +0530

    increment & decriment

---
+++ Ass1.cpp
@@ -0,0 +1,14 @@
+//Write a program in C++ to add two numbers accept through keyboard.//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Enter two intergers to add\n";
+    cin >>a >>b;
+
+    sum = a + b;
+    cout <<"Sum of the numbers;" << sum <<endl;
+    return 0;
+}
+
