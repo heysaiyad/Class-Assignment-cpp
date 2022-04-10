ec4da7add2392f5ce5dfbe97d1e237555e353d4b
heyaadarsh <aadarsh0001.be21@chitkara@edu.in>
2022-04-10 19:34:19 +0530

    Added comments in the code for user's convenience

--- Ass1.cpp
+++ Ass1.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main()
 {
-    int a, b, sum;
+    int a, b, sum; // Taking input from the user!
     cout << "Enter two intergers to add\n";
     cin >>a >>b;
 
     sum = a + b;
-    cout <<"Sum of the numbers: " << sum <<endl;
+    cout <<"Sum of the numbers: " << sum <<endl; // Printing the sum value!
     return 0;
 }
 
