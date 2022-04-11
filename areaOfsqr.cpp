aaf49757467980d30977a25c875665100e9433f1
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2022-04-11 14:58:10 +0530

    Area of square

---
+++ areaOfsqr.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int l;
+    cout<<"Enter the value of l: "<<endl;
+    cin>>l;
+    int area = l *l;
+    cout<<"Area of square is : "<< area<<endl;
+    
+}
